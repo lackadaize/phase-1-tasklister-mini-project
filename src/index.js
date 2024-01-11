2636900abd6fd88167a43bcb8d6bebcdb8ef9cd1
Jonathan Kulak <lackadaize@msn.com>
2024-01-11 10:22:52 -0500

    This is the second commit for the TaskListere Mini-Project assignment.

--- src/index.js
+++ src/index.js
@@ -24,21 +24,3 @@ function populateList(task) {
 function buttonDelete(e) {
   e.target.parentNode.remove();
 }
-
-// A delete function that will remove tasks from your list
-// A priority value selected from a dropdownLinks to an external site. that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
-// As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
-// Ability to edit tasks
-// Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
-
-// As a user, I should be able to type a task into the input field.
-// As a user, I should be able to click some form of a submit button.
-
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   event.preventDefault();
-//   console.log("test");
-//   document.getElementById('create-task-form').addEventListener ("submit", (event) => {
-//     event.preventDefault();
-//     console.log("submitevent");
-//   });
-// });
